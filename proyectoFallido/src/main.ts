4d25d90a7559353275d6ff7dd6261610d8794af4
MitchFrdz <sara.quintero@tectijuana.edu.mx>
2017-10-13 12:01:06 -0700

    Borrar proyecto fallido

--- proyectoFallido/src/main.ts
+++
@@ -1,7 +0,0 @@
-import * as ko from 'knockout';
-import package from 'package'
-
-class ViewModel{
-  words = ko.observableArray(['Hello', 'World']);
-}
-ko.applyBindings(new ViewModel());
--- proyectoFallido/src/require-config.js
+++
@@ -1,6 +0,0 @@
-require.config({
-    paths: {
-        'knockout': 'node_modules/knockout/build/output/knockout-latest'
-    }
-});
-require(['typescript/main']); //run main.ts from here.
